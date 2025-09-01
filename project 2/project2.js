c989892958a6a7c05ff468c8f082b1c740d56e95
sherif0786 <harsithh165@gmail.com>
2025-09-02 00:20:28 +0530

    Add files via upload

---
+++ project 2/project2.js
@@ -0,0 +1,6 @@
+// Contact Form Validation
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for contacting us! We will get back to you soon.");
+  this.reset();
+});
